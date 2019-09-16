48eb1451bc0a892a7ea0e9f09881698854aee573
Ivelisse Mandato <mandatoi@bc.edu>
2019-09-16 17:38:08 -0400

    class dice assignment

--- DungeonDice/ViewController.swift
+++ DungeonDice/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceRolledMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func diceRoll(sides: Int) {
+        var diceRolled = Int.random(in: 1...Int(sides))
+        diceRolledMessage.text = "You rolled a \(sides)-sided dice and rolled a \(diceRolled)"
+    }
+    
 
 
+    @IBAction func fourButtonPressed(_ sender: UIButton) {
+         diceRoll(sides: 4)
+    }
+    
+    @IBAction func sixButtonPressed(_ sender: UIButton) {
+        diceRoll(sides: 6)
+    }
+    
+    @IBAction func eightButtonPressed(_ sender: UIButton) {
+        diceRoll(sides: 8)
+    }
+    
+    @IBAction func tenButtonPressed(_ sender: UIButton) {
+        diceRoll(sides: 10)
+    }
+    
+    @IBAction func twelveButtonPressed(_ sender: UIButton) {
+        diceRoll(sides: 12)
+    }
+    
+    @IBAction func twentyButtonPressed(_ sender: UIButton) {
+        diceRoll(sides: 20)
+    }
+    
+    @IBAction func hundredButtonPressed(_ sender: UIButton) {
+        diceRoll(sides: 100)
+    }
+    
 }
 
